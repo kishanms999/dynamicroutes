7284062c47e5e2bcaf20fec2b1c76f233745bd47
kishanms999 <kishanms99@gmail.com>
2023-04-26 00:35:30 +0530

    sequelize fn added

--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql=require('mysql2');
-
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'root123'
+const Sequelize=require('sequelize');
+const sequelize=new Sequelize('node-complete','root','root123',{
+    dialect:'mysql',
+    host:'localhost'
 });
 
-module.exports=pool.promise();
+module.exports=sequelize;
